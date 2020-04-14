db868e7ac641462dae39dde48a77ca252aa79730
svarvel <varvello@gmail.com>
2020-04-14 12:28:50 -0400

    fixes to make it work on macos, puff puff

--- simple-test.sh
+++ simple-test.sh
@@ -27,11 +27,11 @@ myprint(){
 generate_post_data(){
   cat <<EOF
 	{
-	"uid":"$uid",
-	"test_label": "$label", 
-	"file_size":"$file_size",
-	"cache_hit":"$cache_hit",
-	"x-served-by":"$x_served",
+	"uid":"${uid}",
+	"test_label": "${label}", 
+	"file_size_on_disk":"${file_size}",
+	"cache_hit":"${cache_hit}",
+	"x-served-by":"${x_served}",
 	"age":"${age}",
 	"type":"${type}",
 	"code":"${code}",
@@ -44,8 +44,8 @@ generate_post_data(){
 	"t_redirect":"${t_redirect}",
 	"t_starttransfer":"${t_starttransfer}",
 	"t_total":"${t_total}",
-	"size_download":"${size_downloadl}",
-	"size_download":"${size_header}"
+	"size_download":"${size_download}",
+	"size_header":"${size_header}"
 	}
 EOF
 }
@@ -63,30 +63,33 @@ test_download(){
 
 	# curl download 
 	myprint "[CURL] Destination: $dst Label: $label"
-	timeout $MAX_DURATION curl -v -H "Accept-Encoding: gzip" -H 'user-agent: $UA' -w "@curl-format.txt" -s $dst -o $out_file > ".stats"  2>".headers"
-	curl_stats=`cat ".stats"`
-	type=`cat ".stats" | cut -f 1 | cut -f 2 -d ":"`
-	code=`cat ".stats" | cut -f 2 | cut -f 2 -d ":"`
-	remoteIP=`cat ".stats" | cut -f 3 | cut -f 2 -d ":"`
-	download_speed=`cat ".stats" | cut -f 4 | cut -f 2 -d ":"`
-	t_dns=`cat ".stats" | cut -f 5 | cut -f 2 -d ":"`
-	t_connect=`cat ".stats" | cut -f 6 | cut -f 2 -d ":"`
-	t_appconnect=`cat ".stats" | cut -f 7 | cut -f 2 -d ":"`
-	t_pretransfer=`cat ".stats" | cut -f 8 | cut -f 2 -d ":"`
-	t_redirect=`cat ".stats" | cut -f 9 | cut -f 2 -d ":"`
-	t_starttransfer=`cat ".stats" | cut -f 10 | cut -f 2 -d ":"`
-	t_total=`cat ".stats" | cut -f 11 | cut -f 2 -d ":"`
-	size_download=`cat ".stats" | cut -f 12 | cut -f 2 -d ":"`
-	size_header=`cat ".stats" | cut -f 13 | cut -f 2 -d ":"`
+	curl -v -H "Accept-Encoding: gzip" -H 'user-agent: $UA' -w "@curl-format.txt" -s $dst -o $out_file > ".stats"  2>".headers"
+	type=`cat ".stats" | head -n 1 | cut -f 1 | cut -f 2 -d ":"`
+	code=`cat ".stats" | head -n 1 |cut -f 2 | cut -f 2 -d ":"`
+	remoteIP=`cat ".stats" | head -n 1 |cut -f 3 | cut -f 2 -d ":"`
+	download_speed=`cat ".stats" | head -n 1 |cut -f 4 | cut -f 2 -d ":"`
+	t_dns=`cat ".stats" | head -n 1 |cut -f 5 | cut -f 2 -d ":"`
+	t_connect=`cat ".stats" | head -n 1 | cut -f 6 | cut -f 2 -d ":"`
+	t_appconnect=`cat ".stats" | head -n 1 | cut -f 7 | cut -f 2 -d ":"`
+	t_pretransfer=`cat ".stats" | head -n 1 | cut -f 8 | cut -f 2 -d ":"`
+	t_redirect=`cat ".stats" | head -n 1 | cut -f 9 | cut -f 2 -d ":"`
+	t_starttransfer=`cat ".stats" | head -n 1 | cut -f 10 | cut -f 2 -d ":"`
+	t_total=`cat ".stats" | head -n 1 | cut -f 11 | cut -f 2 -d ":"`
+	size_download=`cat ".stats" | head -n 1 | cut -f 12 | cut -f 2 -d ":"`
+	size_header=`cat ".stats" | head -n 1 | cut -f 13 | cut -f 2 -d ":"`
 	
 	# check return code 
 	cache_hit="N/A" 
 	x_served="N/A"
 	age="N/A"
 	if [ -f ".headers" ] 
-	then 
-		x_served=`cat ".headers" | grep "x-served-by" | awk '{print $NF}' | sed 's/\\r//g'`
-		age=`cat ".headers" | grep "age:" | awk '{print $NF}' | sed 's/\\r//g'`
+	then
+		tr '\r' '\n' < ".headers" > t
+		mv t ".headers"
+		#x_served=`cat ".headers" | grep "x-served-by" | awk '{print $NF}' | sed 's/\\r//g' | sed 's/^M//g' | sed 's/\\n//g'` 
+		x_served=`cat ".headers" | grep "x-served-by" | awk '{print $NF}'`
+		#age=`cat ".headers" | grep "age:" | awk '{print $NF}' | sed 's/\\r//g' | sed 's/^M//g' | sed 's/\\n//g'`
+		age=`cat ".headers" | grep "age:" | awk '{print $NF}'`
 		cat ".headers" | grep "HIT" > /dev/null
 		if [ $? -eq 0 ] 
 		then 
@@ -95,18 +98,32 @@ test_download(){
 			cache_hit="False"
 		fi 
 	fi 
-
+	
 	# logging 
 	t_end=`date +%s`
 	let "t_passed = t_end - t_start"
-	file_size=`stat $out_file | grep "Size"  | cut -f 1 | cut -f 2 -d ":"`
-	myprint "File size:$file_size\tCache-HIT:$cache_hit\tX-served-by:$x_served\tAge:$age"
-	rm $out_file 
+	if [ $isMacOS == "true" ] 
+	then
+		file_size=`stat $out_file  | cut -f 8 -d " "`
+	else 
+		file_size=`stat $out_file | grep "Size"  | cut -f 1 | awk -F ":" '{print int($2)}'`
+	fi 
+	myprint "File:$out_file\t Size:$file_size\tCache-HIT:$cache_hit\tX-served-by:$x_served\tAge:$age"
 
 	# report to Matteo's server in NJ
-	uid=`date +%s`
 	curl  -H "Content-Type:application/json" -X POST -d "$(generate_post_data)" https://nj.batterylab.dev:8080/dCURL
 }
+
+# check for macOS
+isMacOS="false"
+uname -a | grep "Darwin" > /dev/null
+if [ $? -eq 0 ]
+then
+	isMacOS="true"
+fi 
+
+# generate "unique" used ir 
+uid=`date +%s`
 
 # test direct download via Fastly (and report results)
 test_download "https://fastly-dev.brave.info/5mb" "fastly"
